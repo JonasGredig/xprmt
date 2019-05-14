dc053f8b6f1669c1fae07181c354fc0bff448d92
JonasGredig <jongredig@gmail.com>
2019-05-14 14:10:23 +0200

    add basic MainFrame

---
+++ src/ch/jonasgredig/xprmt/Starter.java
@@ -0,0 +1,11 @@
+package ch.jonasgredig.xprmt;
+
+import ch.jonasgredig.xprmt.view.MainFrame;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+
+}
---
+++ src/ch/jonasgredig/xprmt/view/MainFrame.java
@@ -0,0 +1,61 @@
+package ch.jonasgredig.xprmt.view;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame {
+
+    private JLabel title = new JLabel("XPRMT");
+
+    private JLabel xScaleInputTitle = new JLabel("X-Scale");
+    private JLabel yScaleInputTitle = new JLabel("Y-Scale");
+
+    private JTextField xScaleInput = new JTextField();
+    private JTextField yScaleInput = new JTextField();
+
+    private JButton generateButton = new JButton("Generate Picture");
+
+    public MainFrame() {
+        BorderLayout layout = new BorderLayout();
+        setLayout(layout);
+
+        title.;
+
+        add(title, BorderLayout.NORTH);
+
+        JPanel xScalePanel = new JPanel();
+        xScalePanel.setLayout(new BorderLayout());
+        xScalePanel.add(xScaleInputTitle, BorderLayout.NORTH);
+        xScalePanel.add(xScaleInput, BorderLayout.SOUTH);
+
+        JPanel yScalePanel = new JPanel();
+        yScalePanel.setLayout(new BorderLayout());
+        yScalePanel.add(yScaleInputTitle, BorderLayout.NORTH);
+        yScalePanel.add(yScaleInput, BorderLayout.SOUTH);
+
+
+        JPanel scalePanel = new JPanel();
+        BorderLayout scaleLayout = new BorderLayout();
+        scalePanel.add(xScalePanel, BorderLayout.NORTH);
+        scalePanel.add(yScalePanel, BorderLayout.SOUTH);
+
+        add(scalePanel, BorderLayout.CENTER);
+
+        add(generateButton, BorderLayout.SOUTH);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+
+        generateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+}
